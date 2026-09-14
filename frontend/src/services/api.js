49b16aee0806df296c88602a3f0b37cdfee63726
Md Zahid Alam <zahiddohmana1@gmail.com>
2026-09-14 19:49:56 +0530

    Configure production API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://hospital-management-siru.onrender.com",
   withCredentials: true,
 });
 export default api;
