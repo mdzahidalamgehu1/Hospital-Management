d955d3bfafe998c4b42ae170731d1817f396b403
Md Zahid Alam <zahiddohmana1@gmail.com>
2026-09-14 19:36:10 +0530

    added home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -30,6 +30,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
         {/* ================= LOGIN / REGISTER ================= */}
 
