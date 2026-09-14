817ee9b2fad7d623643f85170b69db4ac5451ee4
Md Zahid Alam <zahiddohmana1@gmail.com>
2026-09-15 00:27:35 +0530

    redeployed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hospital-management-siru.onrender.com/api",
   withCredentials: true,
 });
 export default api;
