a74bbffc2fd5cb27f117e8aa145a54ee45b11573
Md Zahid Alam <zahiddohmana1@gmail.com>
2026-09-15 14:06:12 +0530

    production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -95,8 +95,8 @@ const loginUser = async (req, res) => {
    //store the token in a cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 3600000, // 1 hour
     });
 
