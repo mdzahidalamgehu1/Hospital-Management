2653c1d370346c40009434b09f5b374400b87246
Md Zahid Alam <zahiddohmana1@gmail.com>
2026-09-15 13:53:07 +0530

    production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -38,12 +38,12 @@ const registerUser = async (req, res) => {
       { expiresIn: "1h" }
     );
 
-   res.cookie("token", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  maxAge: 60 * 60 * 1000,
-});
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 3600000,
+    });
 
     res.status(201).json({
       token,
